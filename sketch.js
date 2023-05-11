a22798c3cc817157a4bfcfe46cd529c0264f16b4
thmancuzoDev <77467010+thmancuzoDev@users.noreply.github.com>
2023-05-11 14:55:42 -0300

    New background and image ball - Ball rotate effect

--- sketch.js
+++ sketch.js
@@ -2,31 +2,39 @@ function setup() {
   createCanvas(600, 400);
   gameMusic.loop();
 }
-//Circle info
+//Ball info
 let xCircle=300;
 let yCircle=200;
 let dCircle=35;
 let rCircle=dCircle/2;
+let fireBallImage;
+ let imageX = xCircle - dCircle / 2;
+ let imageY = yCircle - dCircle / 2;
+ let dImage = dCircle;
 
-//Circle speed
+//Ball speed
 let xCircleSpeed=8;
 let yCircleSpeed=8;
 
 //Player base
+
 let wPlayer=10;
 let hPlayer=95;
 
 //Left player info
+
 let xLeftPlayer=8;
 let yLeftPlayer=160;
 let yLeftPlayerSpeed=10;
 
 //Right player info
+
 let xRightPlayer=582;
 let yRightPlayer=160;
 let yRightPlayerSpeed=10
 
 //Game conditions
+let backgroundImage;
 let collide2d = false
 let easeAmount;
 
@@ -39,14 +47,16 @@ let gameMusic;
 let ballSound;
 let scoreSound;
 
-function preload (){
+function preload () {
+  backgroundImage = loadImage('backgroundImage.jpg');
+  fireBallImage = loadImage('fireBall.png');
   gameMusic = loadSound('trilha.mp3');
   ballSound = loadSound('raquetada.mp3');
   scoreSound = loadSound('ponto.mp3');
 }
 
 function draw() {
-  background(0);
+  drawBackground()
   createBall();
   movimentBall();
   screenLimits();
@@ -60,13 +70,42 @@ function draw() {
   playersScore();
 }
 
-function createBall () {
-   circle (xCircle,yCircle,dCircle);
+function drawBackground() {
+  background(0);
+  // Calculate the scaling factor to fit the image proportionally
+  let scale = max(width / backgroundImage.width, height / backgroundImage.height);
+  // Calculate the dimensions of the scaled image
+  let imageWidth = backgroundImage.width * scale;
+  let imageHeight = backgroundImage.height * scale;
+  // Calculate the x and y coordinates to center the image on the canvas
+  let x = (width - imageWidth) / 5;
+  let y = (height - imageHeight) / 5;
+  // Draw the background image
+  image(backgroundImage, x, y, imageWidth, imageHeight);
+}
+
+function createBall() {
+  push();
+  translate(xCircle, yCircle);
+  rotate(random(TWO_PI)); // Random rotation angle between 0 and 2*PI
+  imageMode(CENTER);
+  image(fireBallImage, 0, 0, dImage, dImage);
+  pop();
 }
 
 function movimentBall () {
   xCircle += xCircleSpeed;
   yCircle += yCircleSpeed;
+}
+
+function resetBallPosition() {
+  xCircle = width / 2;
+  yCircle = height / 2;
+  
+  let angle = random(-PI / 6, PI / 4); // Adjust the angle range as desired
+  let speed = 5; // Adjust the speed as desired
+  xCircleSpeed = speed * Math.cos(angle) * (random() > 0.5 ? 1 : -1);
+  yCircleSpeed = speed * Math.sin(angle) * (random() > 0.5 ? 1 : -1);
 }
 
 function screenLimits () {
@@ -85,18 +124,19 @@ function createPlayer (x,y) {
 }
 
 function controlLeftPlayer () {
-  if (keyIsDown(UP_ARROW)) {
+  if (keyIsDown(UP_ARROW) && yLeftPlayer > 0) {
     yLeftPlayer -= yLeftPlayerSpeed;
   }
 
-  if (keyIsDown(DOWN_ARROW)) {
+  if (keyIsDown(DOWN_ARROW) && yLeftPlayer + hPlayer < height) {
     yLeftPlayer += yLeftPlayerSpeed;
   }
 }
 
 function computerInteligence () {
   let targetY = yCircle - hPlayer / 2;
- let easeAmount = 0.1;
+  let easeAmount = 0.1;
+  targetY = constrain(targetY, 0, height - hPlayer);
   yRightPlayer += (targetY - yRightPlayer) * easeAmount;
 }
 
@@ -108,6 +148,11 @@ function libraryColision (x,y) {
     // Increase the ball's speed
     xCircleSpeed *= 1.1; // Adjust the speed increment as desired
     yCircleSpeed *= 1.1; // Adjust the speed increment as desired
+    // Increase an ball effect 
+    let playerOffset = yLeftPlayer - yRightPlayer;
+    let speedOffset = Math.abs(xCircleSpeed) + Math.abs(yCircleSpeed);
+    yCircleSpeed += playerOffset * 0.002 * speedOffset;
+
     ballSound.play();
   }
 }
@@ -133,14 +178,4 @@ function playersScore() {
     if (xCircle-rCircle<1) {
       scoreRight += 1; scoreSound.play(); resetBallPosition();
     }
-}
-
-function resetBallPosition() {
-  xCircle = width / 2;
-  yCircle = height / 2;
-  
-  let angle = random(-PI / 6, PI / 3); // Adjust the angle range as desired
-  let speed = 8; // Adjust the speed as desired
-  xCircleSpeed = speed * Math.cos(angle) * (random() > 0.5 ? 1 : -1);
-  yCircleSpeed = speed * Math.sin(angle) * (random() > 0.5 ? 1 : -1);
 }
